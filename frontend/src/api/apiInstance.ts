ead3c0cd6269099fcfc33462d7d3824f1dcf9769
Gonzalo Perez <gonzalo.anuar13@outlook.com>
2025-10-06 20:16:10 -0600

    refactor: update API base URL to use environment variable and remove ErrorBoundary component

--- frontend/src/api/apiInstance.ts
+++ frontend/src/api/apiInstance.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_API_URL
+console.log(BASE_URL)
 export const apiNote = axios.create({
-  baseURL: 'http://localhost:5001/api/',
+  baseURL: `${BASE_URL}/api/`,
 });
